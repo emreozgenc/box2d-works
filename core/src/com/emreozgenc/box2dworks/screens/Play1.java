76d7f8c8f4a8674adfe8eb118ad0de67243681eb
emreozgenc <emreozgenc@hotmail.com.tr>
2020-07-25 22:04:14 +0300

    Added bouncing ball

--- core/src/com/emreozgenc/box2dworks/screens/Play1.java
+++ core/src/com/emreozgenc/box2dworks/screens/Play1.java
@@ -1,12 +1,18 @@
 package com.emreozgenc.box2dworks.screens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
 import com.emreozgenc.box2dworks.Box2DWorks;
 import com.emreozgenc.box2dworks.Constants;
@@ -35,6 +41,44 @@ public class Play1 implements Screen {
         batch = new SpriteBatch();
 
         ballTexture = new Texture(Gdx.files.internal("ball.png"));
+        initBodies();
+    }
+
+    private void initBodies() {
+
+        // Ball body define
+        BodyDef bodyDef = new BodyDef();
+        bodyDef.type = BodyDef.BodyType.DynamicBody;
+        bodyDef.position.set(new Vector2(3, 3));
+        bodyDef.fixedRotation = false;
+
+        ballBody = world.createBody(bodyDef);
+
+        CircleShape shape = new CircleShape();
+        shape.setRadius(Constants.BALL_RADIUS);
+
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.shape = shape;
+        fixtureDef.restitution = 0.6f;
+
+        ballBody.createFixture(fixtureDef);
+        shape.dispose();
+
+        // Ground body define
+        bodyDef = new BodyDef();
+        bodyDef.position.set(new Vector2(8, 1));
+        bodyDef.type = BodyDef.BodyType.StaticBody;
+
+        groundBody = world.createBody(bodyDef);
+
+        PolygonShape polShape = new PolygonShape();
+        polShape.setAsBox(8, 1);
+
+        fixtureDef = new FixtureDef();
+        fixtureDef.shape = polShape;
+
+        groundBody.createFixture(fixtureDef);
+        polShape.dispose();
     }
 
     private void update(float delta) {
@@ -43,11 +87,17 @@ public class Play1 implements Screen {
     }
 
     private void handleInput() {
-
+        if(Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) {
+            ballBody.applyLinearImpulse(new Vector2(0, 12),
+                    ballBody.getWorldCenter(),
+                    true);
+        }
     }
 
     private void render2(SpriteBatch batch) {
-        batch.draw(ballTexture, 3, 3,
+        batch.draw(ballTexture,
+                ballBody.getPosition().x - Constants.BALL_RADIUS,
+                ballBody.getPosition().y - Constants.BALL_RADIUS,
                 Constants.BALL_RADIUS * 2,
                 Constants.BALL_RADIUS * 2);
     }
