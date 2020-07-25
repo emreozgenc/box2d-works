55d4a4c1c486565912b80469e4e5ffa2566075fe
emreozgenc <emreozgenc@hotmail.com.tr>
2020-07-25 21:08:45 +0300

    Changed desktop config file

--- desktop/src/com/emreozgenc/box2dworks/desktop/DesktopLauncher.java
+++ desktop/src/com/emreozgenc/box2dworks/desktop/DesktopLauncher.java
@@ -3,10 +3,14 @@ package com.emreozgenc.box2dworks.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.emreozgenc.box2dworks.Box2DWorks;
+import com.emreozgenc.box2dworks.Constants;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = Constants.WIDTH;
+		config.height = Constants.HEIGHT;
+		config.resizable = false;
 		new LwjglApplication(new Box2DWorks(), config);
 	}
 }
