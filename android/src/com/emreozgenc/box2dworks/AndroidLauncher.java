69db56b6137691078ad17bad30e9c799504b78e6
emreozgenc <emreozgenc@hotmail.com.tr>
2020-07-25 20:51:08 +0300

    Init project

---
+++ android/src/com/emreozgenc/box2dworks/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.emreozgenc.box2dworks;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.emreozgenc.box2dworks.Box2DWorks;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Box2DWorks(), config);
+	}
+}
---
+++ desktop/src/com/emreozgenc/box2dworks/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.emreozgenc.box2dworks.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.emreozgenc.box2dworks.Box2DWorks;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Box2DWorks(), config);
+	}
+}
